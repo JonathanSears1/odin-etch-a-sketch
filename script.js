40235c221194a79cbc1e55b6064a05f33050ac7c
Jonathan Sears <jonathansears7@gmail.com>
2023-12-30 00:22:12 -0500

    added options menu to UI

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 const container = document.querySelector("#container")
+const reset = document.querySelector("#reset")
+const toggleEraser = document.querySelector("#eraser")
+const userGridSize = document.querySelector("#user_grid_size")
+const changeSize = document.querySelector("#size")
+let eraser = false
+let mouse = false
+let gridSize = 16
+toggleEraser.addEventListener("click",() => {
+    if (eraser == true) {
+        console.log(eraser)
+        eraser = false
+        toggleEraser.setAttribute("background-color","#707070")
+    }
+    else {
+        console.log(eraser)
+        eraser = true
+        toggleEraser.setAttribute("background-color","#b02e2e")
+    }
+})
+container.addEventListener("mousedown", () => mouse = true)
+container.addEventListener("mouseup", ()  => mouse = false)
 
-console.log("hello world")
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div")
-    row.className = "row"
-    row.style.height = "6.25%"
-    row.style.width = "100%"
-    row.style.display = "flex"
-    container.appendChild(row)
-    for (let i = 0; i < 16; i++) {
-        const square = document.createElement("div")
-        square.className = "square"
-        square.style.width = "25%"
-        square.style.height = "100%"
-        square.style.flex = "auto"
-        square.addEventListener("mouseover",() => square.style.backgroundColor = "red")
-        square.addEventListener("mouseleave", () => square.style.backgroundColor = "blue")
-        row.appendChild(square)
+function createGrid() {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div")
+        row.className = "row"
+        row.style.height = "6.25%"
+        row.style.width = "100%"
+        row.style.display = "flex"
+        container.appendChild(row)
+        for (let i = 0; i < gridSize; i++) {
+            const square = document.createElement("div")
+            square.className = "square"
+            square.style.width = "25%"
+            square.style.height = "100%"
+            square.style.flex = "auto"
+            square.style.border = "dashed"
+            square.style.borderWidth = "1px"
+            square.addEventListener("mouseover",() => {
+                if (eraser) {
+                    if (mouse) {square.style.backgroundColor = "#c7c7c7"}
+                }
+                else {
+                    if (mouse) {square.style.backgroundColor = "black"}
+                }
+            })
+            row.appendChild(square)
+        }
     }
 }
+function deleteGrid() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+createGrid()
+reset.addEventListener("click", () => {deleteGrid(); createGrid()})
+changeSize.addEventListener("click", () => {
+    gridSize = userGridSize.value
+    deleteGrid()
+    createGrid()
+})
